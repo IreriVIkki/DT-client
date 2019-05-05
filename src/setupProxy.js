5e0c267a3d734c92c1ca185d298c6cb22c1c667d
IreriVikki <wambsviki@gmail.com>
2019-05-06 01:20:40 +0300

    sdf

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,5 @@
-// const proxy = require("http-proxy-middleware");
+const proxy = require("http-proxy-middleware");
 
-// module.exports = function(app) {
-//     app.use(proxy("/api", { target: "http://18.224.69.38:80" }));
-// };
+module.exports = function(app) {
+    app.use(proxy("/api", { target: "http://18.224.69.38:80" }));
+};
