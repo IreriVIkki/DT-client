af047080b04341f6f28b94b955082bf1833d5cd9
IreriVikki <wambsviki@gmail.com>
2019-05-06 04:37:47 +0300

    Updates

---
+++ precache-manifest.c0c6ce35c01ad700ce49a42c650281c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "443293d56b86e700ca09d00edb4e2b9e",
+    "url": "/DT-client/static/media/loader.443293d5.gif"
+  },
+  {
+    "revision": "e7a3868f2507befd8788",
+    "url": "/DT-client/static/js/runtime~main.ebc0cefd.js"
+  },
+  {
+    "revision": "dc9dc5da7327c465fd3a",
+    "url": "/DT-client/static/js/main.9836eaab.chunk.js"
+  },
+  {
+    "revision": "6e9fa6581451d610fa8f",
+    "url": "/DT-client/static/js/2.ab357b74.chunk.js"
+  },
+  {
+    "revision": "dc9dc5da7327c465fd3a",
+    "url": "/DT-client/static/css/main.b8d3a770.chunk.css"
+  },
+  {
+    "revision": "4138e29e568f116156287b0e9872677b",
+    "url": "/DT-client/index.html"
+  }
+];
